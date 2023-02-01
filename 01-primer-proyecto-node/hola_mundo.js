55d923e9f716a7e9896a968399856af95119fc30
eruotolo <edgardoruotolo@outlook.com>
2023-02-01 13:23:11 -0300

    Primer Commit del Curso

---
+++ 01-primer-proyecto-node/hola_mundo.js
@@ -0,0 +1,1 @@
+console.log("Hola Mundo Prueba de Mensaje en Pantalla");
---
+++ 01-primer-proyecto-node/index.js
@@ -0,0 +1,9 @@
+// Mensaje de Bienvenida
+console.log('Esta es la puerta de entrada al proyecto');
+
+/*
+    LINEAS DE COMENTARIOS
+*/
+
+// Este va ser el mensaje que se envía para despedirse
+console.log('Adios');
